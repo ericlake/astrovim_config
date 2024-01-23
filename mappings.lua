e27b9118f1ebadbde3b0af8bda7f37c2114f0bb3
Eric Lake <ericlake@gmail.com>
2024-01-23 16:05:25 -0600

    update configs
    Add colorschemes
    Update formatting

--- mappings.lua
+++ mappings.lua
@@ -11,8 +11,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(
-          bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -25,8 +26,8 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-    ["<leader>gB"] = { "<cmd>ToggleBlame virtual<cr>", desc = "Toggle git blame"},
-    ["<leader>Ni"] = { "<cmd>Neorg index<cr>", desc="Go to Neorg index file"}
+    ["<leader>gB"] = { "<cmd>ToggleBlame virtual<cr>", desc = "Toggle git blame" },
+    ["<leader>Ni"] = { "<cmd>Neorg index<cr>", desc = "Go to Neorg index file" },
   },
   t = {
     -- setting a mapping to false will disable it
--- plugins/community.lua
+++ plugins/community.lua
@@ -1,13 +1,22 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- Add dracula and dracula-soft options to the colorscheme list.
-  { import = "astrocommunity.colorscheme.dracula-nvim" },
+
+  -- Add the ability to quickly see the git blame output
   { import = "astrocommunity.git.blame-nvim" },
+
+  -- Add nvim trouble debugging tool
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  -- Add colorschemes.
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
   -- { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
 }
